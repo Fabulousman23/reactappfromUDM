ee1c848fd24245c2f632ac0f0518166fc38a4382
Fabulousman23 <andrew.makiev@gmail.com>
2022-07-27 08:49:40 -0400

    adding js code to ExpenseItem

--- src/componetns/expense-item/ExpenseItem.js
+++ src/componetns/expense-item/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 7, 25);
+    const expenseTitle = 'iPad';
+    const expenseAmount = 939.46;
+
     return (
         <div className="expense-item">
-            <div>August 25th 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>iPad</h2>
-                <div className='expense-item__price'>$939.46</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     );
