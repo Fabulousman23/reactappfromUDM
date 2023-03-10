4c81b88e4737522ab3b64e803bb24250c56d14ca
Fabulousman23 <andrew.makiev@gmail.com>
2023-03-10 15:06:32 -0500

    create add new expenses button

--- src/componetns/NewExpense/NewExpense.js
+++ src/componetns/NewExpense/NewExpense.js
@@ -1,18 +1,35 @@
 import "./NewExpense.css";
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = props => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = enteredExpenseData => {
     const expsenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expsenseData);
+    setIsEditing(false);
+  };
+  const startEditingHadler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHadler}>Add New Expenses</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
